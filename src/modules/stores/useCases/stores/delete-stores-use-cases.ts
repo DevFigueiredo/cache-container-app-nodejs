f2d41b61c7bd066b7f6e472ff0ab4696f68ce978
Daniel Figueiredo <daniel.figueiredo@compasso.com.br>
2022-01-22 11:15:09 -0300

    feat: add usecase delete store

--- src/modules/stores/useCases/stores/delete-stores-use-cases.ts
+++ src/modules/stores/useCases/stores/delete-stores-use-cases.ts
@@ -1,8 +1,14 @@
 import { Store } from '../../../../shared/domain/store'
+import { IRepository } from '../../../../shared/protocols/repositories/repositories'
 import { IUseCase } from '../../../../shared/protocols/useCases/useCases'
 
-export class DeleteStoresUseCase implements IUseCase<Store> {
-  async execute (params?: Store): Promise<Store> {
-    return {} as any
+export class DeleteStoresUseCase implements IUseCase<Store, void> {
+  private readonly storeRepository: IRepository<Store>
+  constructor ({ storeRepository }: any) {
+    this.storeRepository = storeRepository
+  }
+
+  async execute (params: Store): Promise<void> {
+    await this.storeRepository.delete({ where: { id: params.id } })
   }
 }
