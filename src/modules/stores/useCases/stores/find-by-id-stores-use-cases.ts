a147c9834fc7abfe78cc12a76d19d9a30784794e
Daniel Figueiredo <daniel.figueiredo@compasso.com.br>
2022-01-22 11:23:43 -0300

    refactor: change name classe finbyid

--- src/modules/stores/useCases/stores/find-by-id-stores-use-cases.ts
+++ src/modules/stores/useCases/stores/find-by-id-stores-use-cases.ts
@@ -2,7 +2,7 @@ import { Store } from '../../../../shared/domain/store'
 import { IRepository } from '../../../../shared/protocols/repositories/repositories'
 import { IUseCase } from '../../../../shared/protocols/useCases/useCases'
 
-export class FindStoresUseCase implements IUseCase<Store, Store> {
+export class FindByIdStoresUseCase implements IUseCase<Store, Store> {
   private readonly storeRepository: IRepository<Store>
   constructor ({ storeRepository }: any) {
     this.storeRepository = storeRepository
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,7 @@
 import { createContainer, asClass, InjectionMode } from 'awilix'
 import { StoreRepository } from '../../modules/stores/infra/knex/repositories/stores/stores-repository-mysql'
 import { DeleteStoresUseCase } from '../../modules/stores/useCases/stores/delete-stores-use-cases'
+import { FindByIdStoresUseCase } from '../../modules/stores/useCases/stores/find-by-id-stores-use-cases'
 import { FindStoresUseCase } from '../../modules/stores/useCases/stores/find-stores-use-cases'
 import { SaveStoresUseCase } from '../../modules/stores/useCases/stores/save-stores-use-cases'
 import { UpdateStoresUseCase } from '../../modules/stores/useCases/stores/update-stores-use-cases'
@@ -11,6 +12,7 @@ const container = createContainer({
 
 container.register({
   findStoresUseCase: asClass(FindStoresUseCase),
+  findByIdStoresUseCase: asClass(FindByIdStoresUseCase),
   saveStoresUseCase: asClass(SaveStoresUseCase),
   deleteStoresUseCase: asClass(DeleteStoresUseCase),
   updateStoresUseCase: asClass(UpdateStoresUseCase),
