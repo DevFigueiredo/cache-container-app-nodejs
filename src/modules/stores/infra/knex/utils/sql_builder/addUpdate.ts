0b117bf7c4d3e88ad7d5d2159f1b2338010bfa33
Daniel Figueiredo <daniel.figueiredo@compasso.com.br>
2022-01-24 20:48:29 -0300

    chore: change sql_builder update to update just with have wheres

--- src/modules/stores/infra/knex/utils/sql_builder/addUpdate.ts
+++ src/modules/stores/infra/knex/utils/sql_builder/addUpdate.ts
@@ -1,9 +1,12 @@
 import { Knex } from 'knex'
 
-export function addUpdate<T> (
-  object: T
+export function addUpdate<Object, TypeWhere> (
+  object: Object,
+  where: Partial<TypeWhere>
 ): (query: Knex.QueryBuilder) => Knex.QueryBuilder {
   return (query: Knex.QueryBuilder): Knex.QueryBuilder => {
+    if (!where) { return query }
+
     return query.update(object)
   }
 }
